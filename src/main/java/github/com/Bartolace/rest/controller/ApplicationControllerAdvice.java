d8d451ee2762729504cbc8d28446f81deecebd54
Bartolace <gabrielll.freitas@hotmail.com>
2022-11-23 00:31:45 -0300

    teste

--- src/main/java/github/com/Bartolace/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/github/com/Bartolace/rest/controller/ApplicationControllerAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.*;
-
+//teste
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 

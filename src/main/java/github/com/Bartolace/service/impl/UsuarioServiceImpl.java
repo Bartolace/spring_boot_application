3c3d80543762c9dd2d37b437e61ac70d1b57b1bf
Gabriel Bartolace <gabrielbartolace@castgroup.com.br>
2022-12-13 16:27:40 -0400

    security

---
+++ src/main/java/github/com/Bartolace/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,45 @@
+package github.com.Bartolace.service.impl;
+
+import github.com.Bartolace.domain.entity.Usuario;
+import github.com.Bartolace.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByLogin(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("Usuário não encontrado na base de dados"));
+
+        String[] roles = usuario.isAdmin() ?
+                new String[]{"ADMIN", "USER"} : new String[]{"USER"};
+
+        return User
+                .builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
+                .build();
+    }
+}
